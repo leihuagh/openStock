2c41b4759dfe08d0216834a4b7f149003eab0120
David <davidmchung123@gmail.com>
2018-06-09 12:54:03 -0500

    Enahncement(StackBarGraph.js)
    - Added Legend and changed color of ticks.
    - Changed x axis from venue names to symbols.

--- client/components/StackedBarGraph.js
+++ client/components/StackedBarGraph.js
@@ -23,10 +23,10 @@ export default class StackedBarGraph extends React.Component {
                 .paddingInner(0.1);
         var x1 = d3.scaleBand().padding(0.05);
         var y = d3.scaleLinear().rangeRound([height, 0]);
-        var z = d3.scaleOrdinal().range(["red", "green", "blue", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+        var z = d3.scaleOrdinal().range(["red", "green", "blue"]);
 
         var keys = ['tapeA', 'tapeB', 'tapeC'];
-        x0.domain(this.props.d.map(function(d) { return d['venueName']; }));
+        x0.domain(this.props.d.map(function(d) { return d['mic']; }));
         x1.domain(keys).rangeRound([0, x0.bandwidth()]);
         y.domain([0, d3.max(this.props.d, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
 
@@ -34,7 +34,7 @@ export default class StackedBarGraph extends React.Component {
         .selectAll("g")
         .data(this.props.d)
         .enter().append("g")
-          .attr("transform", function(d) { return "translate(" + x0(d['venueName']) + ",0)"; })
+          .attr("transform", function(d) { return "translate(" + x0(d['mic']) + ",0)"; })
         .selectAll("rect")
         .data(function(d) { return keys.map(function(key) { return {key: key, value: +d[key]}; }); })
         .enter().append("rect")
@@ -51,7 +51,7 @@ export default class StackedBarGraph extends React.Component {
                 
         g.append("g")
             .attr("class", "axis")
-            .call(d3.axisLeft(y).ticks(null, "s"))
+            .call(d3.axisLeft(y).ticks(null, ".2"))
             .append("text")
             .attr("x", 2)
             .attr("y", y(y.ticks().pop()) + 0.5)
@@ -60,6 +60,27 @@ export default class StackedBarGraph extends React.Component {
             .attr("font-weight", "bold")
             .attr("text-anchor", "start")
             .text("Stocks Sold");
+
+        var legend = g.append("g")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .attr("text-anchor", "end")
+          .selectAll("g")
+          .data(keys.slice())
+          .enter().append("g")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      
+        legend.append("rect")
+            .attr("x", width - 19)
+            .attr("width", 19)
+            .attr("height", 19)
+            .attr("fill", z);
+      
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9.5)
+            .attr("dy", "0.32em")
+            .text(function(d) { return d; });
     }
 
     render() {
